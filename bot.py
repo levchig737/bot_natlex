5b193fccd211bd937dd8618c8198ed3fc03211af
levchig737 <levchekarev3@gmail.com>
2023-10-09 10:45:04 +0300

    add test = 113

--- bot.py
+++ bot.py
@@ -22,8 +22,7 @@ def load_token(file_path):
 TOKEN = load_token('TOKEN.txt')
 bot = telebot.TeleBot(TOKEN)
 
-TEST=111
-
+TEST=113
 
 # Получение текущего пути к файлу бота
 bot_directory = os.path.dirname(os.path.abspath(__file__))
