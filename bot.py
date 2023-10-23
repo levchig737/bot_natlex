dcde7e864ca6bbc3ad383bd91917093077c80352
levchig737 <levchekarev3@gmail.com>
2023-10-23 08:38:37 +0300

    add TEST

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ def load_token(file_path):
 
 # Запуск бота
 TOKEN = load_token('TOKEN.txt')
-bot = telebot.TeleBot(TOKEN) 
+bot = telebot.TeleBot(TOKEN)
 
 # Получение текущего пути к файлу бота
 bot_directory = os.path.dirname(os.path.abspath(__file__))
@@ -34,6 +34,8 @@ BASE_DIR = os.path.join(bot_directory, PATH_TO_DOCS)
 ALLOWED_USERS = []
 ALLOWED_ADMINS = []
 # USERS_INFO = {}
+
+TEST = 1231
 
 
 
